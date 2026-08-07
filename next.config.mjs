94ad70e42aa95ca32b2c9c3f4a51d420c1fb41e2
abdullahtariqabbasi <31182342+abdullahtariqabbasi@users.noreply.github.com>
2026-08-08 00:04:21 +0500

    Update next.config.mjs for GitHub Pages support

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  // Required for GitHub Pages
+  output: "export",
+
+  // Required if you use next/image
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
